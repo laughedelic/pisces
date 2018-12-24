9cb1a6a89164a474060d0bb2ee752dd756b0b941
Nicholas Boyle <nickboyle5496@gmail.com>
2018-12-23 19:49:24 -0500

    Removed delay between backspaces (#13)

--- functions/_pisces_backspace.fish
+++ functions/_pisces_backspace.fish
@@ -1,8 +1,16 @@
 function _pisces_backspace -d "Overrides backspace to handle empty pairs removal"
 
-    for pair in $pisces_pairs
-        _pisces_remove (string split -- ',' $pair)
-        and return 0
+    set -l line (commandline | string join \n)
+    set -l index (commandline -C)
+    if [ $index -ge 1 ]
+        set -l char (string sub -s $index -l 1 "$line")
+        for pair in $pisces_pairs
+            set -l var (string split -- ',' $pair)
+            if [ $var[1] = $char ]
+                _pisces_remove $var
+                and return 0
+            end
+        end
     end
 
     commandline -f backward-delete-char
