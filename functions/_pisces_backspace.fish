8d26b0852c2d4ee9983474ec157fef7d878bea1d
Nicholas Boyle <nickboyle5496@gmail.com>
2019-06-16 13:29:43 -0700

    Fix issue with backspacing on lines starting with a dash (#17)

--- functions/_pisces_backspace.fish
+++ functions/_pisces_backspace.fish
@@ -3,7 +3,7 @@ function _pisces_backspace -d "Overrides backspace to handle empty pairs removal
     set -l line (commandline | string join \n)
     set -l index (commandline -C)
     if [ $index -ge 1 ]
-        set -l char (string sub -s $index -l 1 "$line")
+        set -l char (string sub -s $index -l 1 -- "$line")
         for pair in $pisces_pairs
             set -l var (string split -- ',' $pair)
             if [ $var[1] = $char ]
